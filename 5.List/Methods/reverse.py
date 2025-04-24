89a1a80ffb9e61eb4a141d4438395d7969d73a68
rockcee31 <thapliyalrohit31@gmail.com>
2025-04-24 18:30:22 +0530

    DAY 4

--- 5.List/Methods/reverse.py
+++ 5.List/Methods/reverse.py
@@ -5,4 +5,4 @@ list1 = [12,3,4,2]
 list1.reverse()
 print(list1)
 
-''' Reverse method does not return anything '''
+''' Reverse method does not return anything OR  you can not store reverse value anywhere'''
